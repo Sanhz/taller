eaa212dbe8c0bd8a74a80dfb44f6a514956af0f8
“Hector <san.her.hector@gmail.com>
2020-11-13 19:18:40 -0600

    Archivo de nombre

---
+++ hector_sanchez.py
@@ -0,0 +1,9 @@
+class hector:
+	def __init__(self, saludo):
+		self.saludo = saludo
+		self.nombre="Hector"
+		self.cumpleaños = "01/06/1996"
+
+		print(saludo+", mi nombre es "+self.nombre+" y mi cumpleaños es el "+self.cumpleaños)
+
+		
